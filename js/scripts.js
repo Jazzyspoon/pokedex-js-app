5aa44f82eaa892b91e5ce3ccfa99c4f3cc935f5c
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-05 13:41:15 -0700

    Update scripts.js
    corrected indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
-let pokemonList = [];
+let pokemonList = [1,2,3];
 
-let pokemon1 = {name: "Pikachu", hieght: 1, type: "electric"
+let pokemon1 = {
+    name: 'Pikachu', 
+    hieght: 1, 
+    type: 'electric'
 };
-Let Pokemon2 = {name: "Blastois", hieght: 2, type: "water"};
-Let pokemon3 = {name: "Charizard", hieght: 6, type: ["fire", "flying"]};
+Let Pokemon2 = {
+    name: 'Blastois',
+    hieght: 2,
+    type: 'water'
+};
+Let pokemon3 = {
+    name: 'Charizard', 
+    hieght: 6, 
+    type: ['fire', 'flying']
+};
+
+
 
 

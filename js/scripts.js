11b7ce533c57adcb16374428108a4a61b2ea32d2
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-05 13:31:49 -0700

    update with pokemon types for task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-//this is not a real document
+let pokemonList = [];
 
-alert('Hello world');
-
-let favoriteFood = "sushi";
-document.write(favoriteFood) 
-
+let pokemon1 = {name: "Pikachu", hieght: "1m", type: "electric"
+};
+Let Pokemon2 = {name: "Blastois", hieght: "1.6m", type: "water"};
+Let pokemon3 = {name="Charizard", hieght: "1.7m", type: ["fire", "flying"]};
 
 

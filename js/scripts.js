ae935f14fa10ab257311b782a18db458242366ab
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-05 13:46:25 -0700

    Update scripts.js
    corrected array and puctuation

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
-let pokemonList = [1,2,3];
-
-let pokemon1 = {
+let pokemonList = [{
     name: 'Pikachu', 
     hieght: 1, 
     type: 'electric'
-};
-Let Pokemon2 = {
+},{
     name: 'Blastois',
     hieght: 2,
     type: 'water'
-};
-Let pokemon3 = {
+},{
     name: 'Charizard', 
     hieght: 6, 
     type: ['fire', 'flying']
 };
+
+
 
 
 

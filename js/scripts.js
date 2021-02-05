ba6fac0c8f3d4dee267d3aa9dedf5e920af158ab
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-05 15:56:07 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
 let pokemonList = [{
     name: 'Pikachu',
-    hieght: 1,
+    height: 1.2,
     type: ['electric']
 }, {
     name: 'Blastois',
-    hieght: 2,
+    height: 2.0,
     type: ['water']
 }, {
     name: 'Charizard',
-    hieght: 6,
+    height: 6.1,
     type: ['fire', 'flying']
 }];
 
+if (pokemonList.type === 'electric' || pokemonList.height === 1.2) {
+    console.log('Pikachu!');
+} else {
+    console.log('Not pikachu!');
+}
+
+if (pokemonList.type === 'water' || pokemonList.height === 2.0) {
+    console.log('Blastois!');
+} else {
+    console.log('Not Blastois!');
+}
+
+if (pokemonList.type === 'fire' || 'flying' || pokemonList.height === 6.1) {
+    console.log('Charizard!');
+} else {
+    console.log('Not Charizard!');
+}
 
 
 

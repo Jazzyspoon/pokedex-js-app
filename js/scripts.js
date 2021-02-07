6ff9cb7313bf3f3e29fb6fbe5ea905930d6511fa
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-07 11:57:08 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,27 @@ let pokemonList = [{
     type: ['fire', 'flying']
 }];
 
-if (pokemonList.type === 'electric' || pokemonList.height === 1.2) {
-    console.log('Pikachu!');
-} else {
-    console.log('Not pikachu!');
+
+for (let i=0 ; i > pokemonList.length; i++);
+    if (pokemonList[0].type = 'electric' && pokemonList[0].height === 1.2) {
+    document.write('Pikachu (height:1.2)   ');
+    } else {
+    document.write('Not Pikachu!');
 }
 
-if (pokemonList.type === 'water' || pokemonList.height === 2.0) {
-    console.log('Blastois!');
-} else {
-    console.log('Not Blastois!');
+for (let i=0; i < pokemonList.length; i++);
+    if (pokemonList[1] === 'water' || pokemonList[1].height === 2.0) {
+    document.write('Blastois (height:2.0)   ');
+    } else {
+    document.write('Not Blastois!');
 }
 
-if (pokemonList.type === 'fire' || 'flying' || pokemonList.height === 6.1) {
-    console.log('Charizard!');
-} else {
-    console.log('Not Charizard!');
-}
+for (let i=0; i < pokemonList.length; i++);
+    if (pokemonList[2].type = 'fire' || 'flying' && pokemonList[2].height === 6.1) {
+    document.write('Charizard (height:6.1)    ');
+    } else {
+    document.write('Not Charizard!');
+    }   
 
 
 

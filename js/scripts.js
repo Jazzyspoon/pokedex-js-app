5debd4c7a6356e2df8c583bf47b2f80846f90393
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-08 13:04:12 -0700

    made changes based on tutor's advice.
    I am going to spend a lot of time working to grasp this concept.
    updated font to make it more readable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,49 @@
-let pokemonList = [{
-    name: 'Pikachu',
-    height: 1.2,
-    type: ['electric']
-}   , {
-    name: '<p>Blastois</p>',
-    height: 2.0,
-    type: ['water']
-}, {
-    name: '<p class="special_pokemon">Charizard</p>',
-    height: 6.1,
-    type: ['fire', 'flying']
-}];
+let pokemonList = [
+    {
+        name: "Pikachu",
+        height: 1.2,
+        type: ["electric"]
+    },
+    {
+        name: "Blastoise",
+        height: 2.0,
+        type: ["water"]
+    },
+    {
+        name: "<p class='special_pokemon'>Charizard</p>",
+        height: 6.1,
+        type: ["fire", "flying"]
+    }
+];
 
-/*writing out the names and heights*/
-for (let i=0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + '<p> hieght: </p>' + pokemonList[i].height);
-} 
+/*writing out the names and heights. I needed a lot of direction on this. 
+Especially helpful knowing how to embed html into document, as I was struggling with this as well.  */
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 6) {
+        document.write(
+            pokemonList[i].name +
+            " " +
+            "<p>" +
+            "Height: " +
+            pokemonList[i].height +
+            "(Wow that's big!)" +
+            "</p>"
+        );
+    } else {
+        document.write(
+            pokemonList[i].name +
+            " " +
+            "<p>" +
+            "Height: " +
+            pokemonList[i].height +
+            "</p>" +
+            "<br/>"
+        );
+    }
+}
 
 /*attempting to make these grid items*/
- let dataArray = pokemonList; // assuming data from API
-    let numColumns = 4; // set number of columns    
-    let grid = document.getElementById('grid-wrapper');
-
- /*attempting to write that's a big pokemon   
- for (let height < 6.0; height.length < 6.0; height++)  {
-        document.write(pokemonList[i].name + ' hieght: ' + pokemonList[i].height + ' ' + 'thats a tiny pokemon!'); 
-    } else if (let height => 6.1; height > 6.0; height++) {
-        document.write(pokemonList[i].name + ' hieght: ' + pokemonList[i].height + ' ' + 'thats a big pokemon!');  
-    }
-*/
-
-
+let dataArray = pokemonList;
+let numColumns = 3;
+let grid = document.getElementById('grid');
 

12e34575aaa4e0a2ebd913642e3fac817c678c30
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-08 09:02:52 -0700

    added some decoration to css file and html..

--- js/scripts.js
+++ js/scripts.js
@@ -12,28 +12,29 @@ let pokemonList = [{
     type: ['fire', 'flying']
 }];
 
-
-for (let i=0 ; i > pokemonList.length; i++);
+/*attempting to create a list of pokemon here on the DOM, but I am not fully sure these can be considered a loops*/
+for (let i=0 ; i < pokemonList.length; i++);
     if (pokemonList[0].type = 'electric' && pokemonList[0].height === 1.2) {
-    document.write('Pikachu (height:1.2)   ');
+    document.write('<p>Pikachu (height:1.2)');
     } else {
     document.write('Not Pikachu!');
 }
 
 for (let i=0; i < pokemonList.length; i++);
     if (pokemonList[1] === 'water' || pokemonList[1].height === 2.0) {
-    document.write('Blastois (height:2.0)   ');
+    document.write('<p>Blastois (height:2.0)');
     } else {
     document.write('Not Blastois!');
 }
 
 for (let i=0; i < pokemonList.length; i++);
     if (pokemonList[2].type = 'fire' || 'flying' && pokemonList[2].height === 6.1) {
-    document.write('Charizard (height:6.1)    ');
+    document.write('<p class="special_pokemon">Charizard (height:6.1)');
     } else {
     document.write('Not Charizard!');
-    }   
+    }  
 
+     
 
 
 

08574f64c87125785b6bb4b8b32be8e1c1bfad7a
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-08 11:40:31 -0700

    still attempting to finish the tasks in 1.3
    I am stuck on creating a loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,30 @@ let pokemonList = [{
     name: 'Pikachu',
     height: 1.2,
     type: ['electric']
-}, {
-    name: 'Blastois',
+}   , {
+    name: '<p>Blastois</p>',
     height: 2.0,
     type: ['water']
 }, {
-    name: 'Charizard',
+    name: '<p class="special_pokemon">Charizard</p>',
     height: 6.1,
     type: ['fire', 'flying']
 }];
 
-/*printing out the names*/
+/*writing out the names and heights*/
 for (let i=0; i < pokemonList.length; i++) {
-   document.write(pokemonList[i].name + 'hieght:' + pokemonList[i].height);
-}
+    document.write(pokemonList[i].name + '<p> hieght: </p>' + pokemonList[i].height);
+} 
 
+/*attempting to make these grid items*/
+ let dataArray = pokemonList; // assuming data from API
+    let numColumns = 4; // set number of columns    
+    let grid = document.getElementById('grid-wrapper');
 
+ /*attempting to write that's a big pokemon*/   
+ for (let height = 6.0; height.length > 6.0; height++)  {
+        document.write(pokemonList[i].name + ' hieght: ' + pokemonList[i].height + ' ' + 'thats a big pokemon!'); 
+    }
 
 
 

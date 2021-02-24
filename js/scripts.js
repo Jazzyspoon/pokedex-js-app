103b1b6987e988ec26a49ad66f6eb4cb03a9d2d3
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-24 16:16:30 -0700

    centered modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //IIFE instantiated
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=25";
 
   //add pokemon from pokemonList
   function add(pokemon) {
